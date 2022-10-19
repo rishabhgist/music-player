7756d906cd84ad3072c3983507e81fbd8e8a25f7
Rishabh Gist <damyrachit@gmail.com>
2022-10-19 21:20:49 +0530

    added play pause function

---
+++ script.js
@@ -0,0 +1,25 @@
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextbtn = document.getElementById('next');
+
+let isPlaying = false;
+
+// Play 
+function playSong() {
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+// Pause
+function pauseSong() {
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+// Play or Pause
+playBtn.addEventListener('click', () => (isPlaying) ? pauseSong() : playSong());
